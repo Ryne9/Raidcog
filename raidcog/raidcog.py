abbda594d1f7e9671d8ba5984bf29a74a06fedfc
ryneG <cupcakeraptor@gmail.com>
2017-11-11 18:46:07 -0500

    FOMRATTING

--- raidcog/raidcog.py
+++ raidcog/raidcog.py
@@ -53,7 +53,7 @@ class raidcog:
                 description += str(date.strftime(self.fmt)) + " " + raid['timezone'] + "\n"
                 for members in raid['members']:
                     if members['id'] == raid['members'][0]['id']:
-                        description += "** - " + members['name'] + " (Raid Leader)\n"
+                        description += " - " + members['name'] + " (Raid Leader)\n"
                     else:
                         description += " - " + members['name'] + "\n"
                 description += "\n"
