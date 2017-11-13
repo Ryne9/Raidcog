96c5178ca04d32db17505fa3a3ace68fd4148fd2
ryneG <cupcakeraptor@gmail.com>
2017-11-12 20:32:07 -0500

    Adds clan banner to clancog

--- clancog/clancog.py
+++ clancog/clancog.py
@@ -14,5 +14,12 @@ class clancog:
         #Your code will go here
         await self.bot.say("https://www.bungie.net/en/ClanV2/Chat?groupId=2762856")
 
+    @commands.command()
+    async def squad(self, context):
+        """Thunderdoges D2 clan banner!"""
+
+        #Your code will go here
+        await self.bot.send_file(context.message.channel, 'data/clancog/raid.png')
+
 def setup(bot):
     bot.add_cog(clancog(bot))
