2322f49b2fb078f54390aa189c09131b00ff2af3
ryneG <cupcakeraptor@gmail.com>
2017-11-12 23:43:27 -0500

    Fixed thunderutil getchannel

--- clancog/clancog.py
+++ clancog/clancog.py
@@ -10,15 +10,11 @@ class clancog:
     @commands.command()
     async def clan(self):
         """Thunderdoges D2 clan link!"""
-
-        #Your code will go here
         await self.bot.say("https://www.bungie.net/en/ClanV2/Chat?groupId=2762856")
 
     @commands.command(pass_context=True)
     async def squad(self, context):
         """Thunderdoges D2 clan banner!"""
-
-        #Your code will go here
         await self.bot.send_file(context.message.channel, 'data/clancog/raid.png')
 
 def setup(bot):
