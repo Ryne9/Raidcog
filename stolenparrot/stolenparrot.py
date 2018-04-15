70c90a22108e89eedbda69ced66a6f420c9ddba2
Ryne <rynegipson@gmail.com>
2018-04-14 21:17:21 -0400

    so immature

--- stolenparrot/stolenparrot.py
+++ stolenparrot/stolenparrot.py
@@ -25,9 +25,9 @@ class stolenparrot:
         reply = await self.bot.wait_for_message(timeout=30, author=author, channel=author_channel)
 
         if reply is None:
-            await self.bot.send_message(author, "Okay, fine.")
+            await self.bot.send_message("Okay, fine.")
         else:
-            self.bot.send_message(author, ":" + reply)
+            self.bot.send_message("HAHAHA WHAT U SED :" + reply.content)
 
 def setup(bot):
     bot.add_cog(stolenparrot(bot))
