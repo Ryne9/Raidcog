6f24f441cc9d0244d9ae31e40db5bf8859ce48fa
Ryne9 <rynegipson@gmail.com>
2018-05-09 03:33:52 -0400

    Change to weird python formatting

--- raidcog/raidcog.py
+++ raidcog/raidcog.py
@@ -167,7 +167,7 @@ class raidcog:
     # ---Time processing and validation---
         in_time = time_msg.content
 
-        if ":" not in in_time or not len(in_time) in [3, 4]:
+        if ":" not in in_time or len(in_time) not in [3, 4]:
             await self.bot.send_message(author,
                                         "Check your time formatting.")
             return
