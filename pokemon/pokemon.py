ea2e25f465b4290caf85ebf09060a64eebebbf2d
Ryne9 <rynegipson@gmail.com>
2018-06-19 19:30:58 -0400

    Correct json formatting

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -38,29 +38,28 @@ class pokemon:
 
     @_pokemon.command(pass_context=True, name='create')
     async def _create(self, context):
-        with open('data/pokemon/pokemon.json') as rawPokemon:
+        with open('data/pokemon.json') as rawPokemon:
             pokemonData = json.load(rawPokemon)
+            pokemon = pokemonData[random.randint(1, 150) - 1]
         # with open('data/pokemon/moves.json') as rawMoves:
         #     movesData = json.load(rawMoves)
+        pokemonMoveMax = len(pokemon["moves"]) - 1
 
-        pokemon = pokemonData[random.randint(1, 150) - 1]
-        pokemonMoveMax = len(pokemon.moves) - 1
-
-        pokemon.learnedMoves = [
-            pokemon.moves[random.randint(0, pokemonMoveMax)],
-            pokemon.moves[random.randint(0, pokemonMoveMax)],
-            pokemon.moves[random.randint(0, pokemonMoveMax)],
-            pokemon.moves[random.randint(0, pokemonMoveMax)]
+        pokemon["learnedMoves"] = [
+            pokemon["moves"][random.randint(0, pokemonMoveMax)],
+            pokemon["moves"][random.randint(0, pokemonMoveMax)],
+            pokemon["moves"][random.randint(0, pokemonMoveMax)],
+            pokemon["moves"][random.randint(0, pokemonMoveMax)]
         ]
 
-        pokemon.level = random.randint(1, 100)
+        pokemon["level"] = random.randint(1, 100)
 
-        title = "Oh wow you caught a " + pokemon.name + " (" + pokemon.id + ")"
-        description = "It's level " + pokemon.level + "\n"
-        description += "It knows " + pokemon.learnedMoves[0].name + ",\n"
-        description += pokemon.learnedMoves[1].name + ",\n"
-        description += pokemon.learnedMoves[2].name + ",\n"
-        description += "and " + pokemon.learnedMoves[3].name + ",\n"
+        title = "Oh wow you caught a " + pokemon["name"] + " (" + pokemon["id"] + ")"
+        description = "It's level " + pokemon["level"] + "\n"
+        description += "It knows " + pokemon["learnedMoves"][0]["name"] + ",\n"
+        description += pokemon["learnedMoves"][1]["name"] + ",\n"
+        description += pokemon["learnedMoves"][2]["name"] + ",\n"
+        description += "and " + pokemon["learnedMoves"][3]["name"] + ",\n"
 
         em = discord.Embed(title=title, description=description, color=discord.Color.blue())
         await self.bot.say(embed=em)
