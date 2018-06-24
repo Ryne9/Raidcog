7b842a63f49f9083404ba2f433f723f895d1b307
Ryne9 <rynegipson@gmail.com>
2018-06-24 11:33:57 -0400

    Revert optional sizing

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -110,7 +110,7 @@ class pokemon:
         await self.bot.send_file(context.message.channel, 'data/pokemon/compost.png')
 
     @_pokemon.command(pass_context=True, name='battle')
-    async def _battle(self, context, x: int, y: int):
+    async def _battle(self, context):
         level = random.randint(1, 100)
         pokemon1 = self.pokemonData[random.randint(1, 150) - 1]
         pokemon2 = self.pokemonData[random.randint(1, 150) - 1]
@@ -135,10 +135,6 @@ class pokemon:
         background.paste(plyrbar, (200, 188), plyrbar)
         enmybar = self.makeEnemyBar(pokemon2)
         background.paste(enmybar, (5, 23), enmybar)
-        if x:
-            newImage = Image.new(mode="RGBA", size=(x, y))
-            newImage.paste(background, (0,0))
-            background = newImage
 
         background.save("data/pokemon/compost.png", quality=100)
         await self.bot.send_file(context.message.channel, 'data/pokemon/compost.png')
